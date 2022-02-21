1e45544129ab7eaa0a35df8ff3c8c4ed966197f1
harsha <v.harshavardhan3@gmail.com>
2022-02-21 22:55:33 +0530

    DB server connection with pycharm Tags added to models.py

--- devpage/admin.py
+++ devpage/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from . models import Project,Contact
+from . models import Project,Contact,Tag
 # Register your models here.
 admin.site.register(Project)
 admin.site.register(Contact)
+admin.site.register(Tag)
--- devpage/models.py
+++ devpage/models.py
@@ -2,7 +2,11 @@ from django.db import models
 from cloudinary.models import CloudinaryField
 from captcha.fields import CaptchaField
 from django import forms
-
+class Tag(models.Model):
+    color = models.CharField(max_length=20)
+    text = models.CharField(max_length=20)
+    def __str__(self):
+        return self.text
 # Create your models here.
 class Project(models.Model):
     projectTemplate = CloudinaryField('image')
@@ -10,7 +14,9 @@ class Project(models.Model):
     desc = models.TextField(default="not mentioned")
     projectLink = models.URLField(max_length=200, default="not mentioned")
     created_at = models.DateTimeField(auto_now_add=True)
-
+    tags = models.ForeignKey(Tag,on_delete=models.PROTECT)
+    show = models.BooleanField()
+    pin = models.BooleanField()
     def __str__(self):
         return f'{self.projectTitle} - {self.desc[:15]} ..'
 
